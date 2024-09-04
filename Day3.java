17996d8fbead929aa3b6eba0ae6769ab1b9a274c
Andri <andrikurniawanm.12@gmail.com>
2024-09-04 10:28:54 +0800

    Day3

---
+++ Day3.java
@@ -0,0 +1,9 @@
+public class Day3 {
+    public static void main(String[] args){
+        System.out.println("Nama" + " Andri Kurniawan M.");
+        System.out.println("Nim" + " D0224313");
+        System.out.println("Tujuan masuk Informatika" + " karena minat");
+    }
+    
+    
+}
