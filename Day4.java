cbff70bec0e1f744232a6d17e5d0c4439cb0f58d
Andri <andrikurniawanm.12@gmail.com>
2024-09-05 11:53:30 +0800

    Day4

---
+++ Day4.java
@@ -0,0 +1,13 @@
+public class Day4 {
+    public static void main(String[] args){
+        System.out.print("Izinkan ");
+        System.out.print("saya ");
+        System.out.print("memperkenalkan diri\n");
+        System.out.printf("Nama saya %s", "Andri\n");
+        System.out.printf("Umur saya %d\n", 20);
+        System.out.printf("Saya dari Informatika %c", 'B');
+        
+    
+}
+    
+}
