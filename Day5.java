454f682b91ed4ea1a017f078ee8a350d4ac5d841
Andri <andrikurniawanm.12@gmail.com>
2024-09-06 14:38:03 +0800

    Day5

---
+++ Day5.java
@@ -0,0 +1,27 @@
+public class Day5 {
+    //Membuat sebuah kelas bernama Day5. Ini seperti memberi nama folder atau file yang akan berisi semua kode.
+    public static void main(String[] args){
+        //Ini adalah bagian penting dari program Java. Semua program dimulai dari sini. Bayangkan ini seperti pintu utama untuk menjalankan program.
+        System.out.print("Izinkan ");
+        System.out.print("saya ");
+        //Menggunakan print untuk menulis kata-kata tanpa pindah ke baris baru.
+        System.out.print("memperkenalkan diri\n");
+        // \n di akhir memperkenalkan diri\n adalah perintah untuk pindah ke baris baru.
+        System.out.printf("Nama saya %s", "Andri\n");
+        //Menggunakan printf untuk mencetak teks dengan format tertentu.
+        System.out.printf("Umur saya %d\n", 20);
+        /*%s (string) adalah tempat untuk String.
+        %d (integer) untuk menampilkan angka (digit).
+        */
+        System.out.printf("Saya dari Informatika %c", 'B');
+        /*
+        %c adalah tempat untuk karakter tunggal seperti tempat khusus yang menunggu diisi oleh satu huruf atau simbol
+        Gunakan petik dua (" ") untuk teks atau kata-kata (String).
+        Gunakan petik satu (' ') untuk satu karakter saja (char).
+        print: Mencetak teks tanpa baris baru di akhir. (1 baris)
+        printf: Mencetak teks dengan format yang ditentukan oleh pengguna. (gaya khusus, angka, teks format)
+        */
+    
+}
+    
+}
