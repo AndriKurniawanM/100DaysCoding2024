93e0119ac55665254cebc295d950267ceaa3fa45
Andri <andrikurniawanm.12@gmail.com>
2024-09-07 14:25:11 +0800

    Day6

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Day6 {
+    public static void main(String[] args) {
+        //Deklarasi Variabel
+        double beratbadan;
+        float tinggibadan;
+        char gologandarah;
+        boolean hujan;
+        
+    }
+}
