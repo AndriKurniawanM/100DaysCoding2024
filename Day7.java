85e7fe7c37ebe5f8ccd36d34f82a72c574c6be63
Andri <andrikurniawanm.12@gmail.com>
2024-09-08 09:22:09 +0800

    Day7

---
+++ Day7.java
@@ -0,0 +1,14 @@
+public class Day7 {
+    public static void main(String[] args) {
+        //Deklarasi dan Inisialisasi Variabel
+        double beratbadan = 50.3;
+        System.out.println("Berat Badan: " + beratbadan);
+        float tinggibadan = 1.65f;
+        System.out.println("Tinggi Badan: " + tinggibadan);
+        char golongandarah = 'B';
+        System.out.println("Golongan Darah: " + golongandarah );
+        boolean hujan = true;
+        System.out.println("Apakah Hujan? " + hujan);
+        
+    }
+}
