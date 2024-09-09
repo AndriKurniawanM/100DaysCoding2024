398e264ca507a32476412ee082cdb0ec3229c461
Andri <andrikurniawanm.12@gmail.com>
2024-09-09 11:10:53 +0800

    Day8

---
+++ Day8.java
@@ -0,0 +1,30 @@
+public class Day8 {
+    public static void main(String[] args) {
+        double beratbadan = 50.3;
+        System.out.println("Sebelum Update: " + beratbadan);
+        //update
+        beratbadan = 53.9;
+        System.out.println("Setelah Update: " + beratbadan);
+        float tinggibadan = 1.65f;
+        System.out.println("Sebelum Update: " + tinggibadan);
+        //update
+        tinggibadan = 1.70f;
+        System.out.println("Setelah Update: " + tinggibadan);
+        char golongandarah = 'B';
+        System.out.println("Sebelum Update: " + golongandarah );
+        //update
+        golongandarah = 'A';
+        System.out.println("Setelah Update: " + golongandarah);
+        boolean hujan = true;
+        System.out.println("Sebelum Update: " + hujan);
+        //update
+        hujan = false;
+        System.out.println("Setelah Update: " + hujan);
+        String nama = "Andri";
+        System.out.println("Sebelum Update: " + nama);
+        nama = "Aika";
+        System.out.println("Setelah Update: " + nama);
+        
+    }
+    
+}
