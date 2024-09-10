e0c93568f05f6150de378981bce823acc1ec33fb
Andri <andrikurniawanm.12@gmail.com>
2024-09-10 14:49:05 +0800

    Day9

---
+++ Day9.java
@@ -0,0 +1,16 @@
+
+public class Day9 {
+    public static void main(String[] args) {
+        final double phi = 3.14;
+        final int kelas = 1;
+        final float ipk = 12.5f;
+        final long panjang = 20000;
+        final byte umur = 20;
+        System.out.println(umur);
+        System.out.println(panjang);
+        System.out.println(ipk);
+        System.out.println(kelas);
+        System.out.println(phi);
+    }
+    
+}
