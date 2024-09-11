a4b4b9685dda1a254c279dd8cabd89956729d7e7
Andri <andrikurniawanm.12@gmail.com>
2024-09-12 00:10:09 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author andri
- */
 public class Day11 {
-    
+    public static void main(String[] args) {
+        short kelas = 32767;
+        System.out.println(kelas);
+    }
 }
