f46c995b729edffd5b1344e9d627f5587e35fe23
Andri <andrikurniawanm.12@gmail.com>
2024-09-14 10:07:54 +0800

    Day13

---
+++ Teskuis.java
@@ -0,0 +1,25 @@
+public class Teskuis {
+    public static void main(String[] args) {
+        System.out.printf("\nBiodata Mahasiswa 1");
+        byte var1 = 19;
+        System.out.printf("\nNama               : Ai");
+        System.out.printf("\nUmur               : " + var1 + " Tahun");
+        short var2 = 100;
+        System.out.printf("\nUmurNenek          : " + var2 + " Tahun");
+        int var3 = 120;
+        System.out.printf("\nUmurKakek          : " + var3 + " Tahun");
+        System.out.printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.printf("\nBiodata Mahasiswa 2");
+        System.out.printf("\nNama               : Kay");
+        var1 = 20;
+        var2 = 101;
+        var3 = 118;
+        System.out.printf("\nUmur               : " + var1 + " Tahun");
+        System.out.printf("\nUmurKakek          : " + var2 + " Tahun");
+        System.out.printf("\nUmurNenek          : " + var3 + " Tahun");
+        System.out.printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        
+
+    
+    }  
+}
