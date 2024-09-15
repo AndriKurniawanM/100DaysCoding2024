4a6c0366731c7eb99433a3cae66048b9148fa6e1
Andri <andrikurniawanm.12@gmail.com>
2024-09-15 20:00:36 +0800

    Day14

---
+++ Day14.java
@@ -0,0 +1,14 @@
+public class Day14 {
+    public static void main(String[] args) {
+        long var1 = 3000000;
+        System.out.println("UKT : " + var1);
+        /*Saya mendeklarasikan sebuah variabel bernama var1 dengan tipe data long yang diberi nilai
+        3.000.000 , tipe data long digunakan untuk menampung nilai numerik yang sangat besar 
+        dari -9,223,372,036,854,775,808 sampai 9,223,372,036,854,775,807 
+        Sembilan quintiliun dua ratus dua puluh tiga kuadriliun tiga ratus tujuh puluh dua triliun tiga puluh enam miliar delapan ratus lima puluh empat juta tujuh ratus tujuh puluh lima ribu delapan ratus delapan.
+        dengan kapasitas 8 byte 64 bit
+        Kemudian variabel tadi saya panggil dengan sout 
+        */
+    }
+}
+ 
