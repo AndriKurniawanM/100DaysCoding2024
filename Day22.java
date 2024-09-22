086e1a2d1d536a9634a3805ab3b0867c76f53e72
Andri <andrikurniawanm.12@gmail.com>
2024-09-23 00:27:33 +0800

    Day22.java

--- Day22.java
+++ Day22.java
@@ -2,12 +2,8 @@ public class Day22 {
     public static void main(String[] args) {
         Integer a = 10;
         Integer b = 20;
-
         Integer c = a + b;
-        
         System.out.println("Hasil penjumlahan: " + c);
-        Integer parsedInt = Integer.parseInt("100");
-        System.out.println("Hasil parseInt: " + parsedInt);
     }
     
 }
