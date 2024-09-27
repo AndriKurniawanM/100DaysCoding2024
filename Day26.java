fd8ba43b94a03d5050087645ba81bbe3e9a78b18
Andri <andrikurniawanm.12@gmail.com>
2024-09-27 22:03:27 +0800

    Day26.java

--- Day26.java
+++ Day26.java
@@ -2,6 +2,6 @@ public class Day26 {
     public static void main(String[] args) {
         Character a;
         a = '1';
-        System.out.println("Stok Bulan Ini : " + a);
+        System.out.println("Stok Bulan Ini: " + a);
     }
 }
