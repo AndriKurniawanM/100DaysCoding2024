1ef3b030afef018a032eeb5160b83834ac1c495e
Andri <andrikurniawanm.12@gmail.com>
2024-09-30 05:10:38 +0800

    Day28

---
+++ Day28.java
@@ -0,0 +1,7 @@
+public class Day28 {
+    public static void main(String[] args) {
+        // Tipe data referensi
+        Boolean a = null;  // bisa bernilai null
+        System.out.println("Tipe referensi: " + a);
+    }
+}
