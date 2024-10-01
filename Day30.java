ad7bf301f4bcb4a2d1db358ed54c1535c1d59976
Andri <andrikurniawanm.12@gmail.com>
2024-10-01 20:20:03 +0800

    Day30

---
+++ Day30.java
@@ -0,0 +1,10 @@
+import java.math.BigDecimal;
+public class Day30 {
+    public static void main(String[] args) {
+         // Inisialisasi BigDecimal
+        BigDecimal angka1 = new BigDecimal("1.5");
+        BigDecimal angka2 = new BigDecimal("0.5");
+        BigDecimal a = angka1.add (angka2);//penjumlahan
+        System.out.println(a);
+    }
+}
