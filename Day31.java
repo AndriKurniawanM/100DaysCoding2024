93164dd4e308525f986f66fae42583fbda558ef4
Andri <andrikurniawanm.12@gmail.com>
2024-10-02 20:10:09 +0800

    Day31

---
+++ Day31.java
@@ -0,0 +1,12 @@
+public class Day31 { //membuat konversi
+    public static void main(String[] args) {
+        String a = "11"; //buat dulu yg ingin dikonversi
+        int b = Integer.parseInt(a); //konversi pake parse 
+        b += 9;
+        System.out.println("Sebelum dikonversi : " + a);
+        System.out.println("Setelah dikonversi : " + b);
+        
+        
+    }
+    
+}
