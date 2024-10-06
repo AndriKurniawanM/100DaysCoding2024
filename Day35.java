05f2a6e31b5933110e8d049cdc1e9f7f99df6c0d
Andri <andrikurniawanm.12@gmail.com>
2024-10-07 07:37:32 +0800

    Day35

---
+++ Day35.java
@@ -0,0 +1,14 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int a,b,c,d,e,f;
+        a = 1;
+        b = 2;
+        c = a+b;
+        System.out.println(a + " + " + b + " = " + c);
+        d = 2;
+        e = 1;
+        f = d-e;
+        System.out.println(d + " - " + e + " = " + f);
+        
+    }
+}
