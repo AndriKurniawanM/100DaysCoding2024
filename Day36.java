869574b179b66760c93f5812e365ca3898dbe45e
Andri <andrikurniawanm.12@gmail.com>
2024-10-07 07:51:58 +0800

    Day36

---
+++ Day36.java
@@ -0,0 +1,17 @@
+public class Day36 {
+    public static void main(String[] args) {    
+    int a,b,c,d,e,f,g,h,i;
+    a = 1;
+    b = 2;
+    c = a*b;
+        System.out.println(a + " * " + b + " = " + c);
+    d = 10;
+    e = 2;
+    f = d/e;
+        System.out.println(d + " / " + e + " = " + f);
+    g = 20;
+    h = 3;
+    i = g%h;
+        System.out.println(g + " % " + h + " = " + i);
+    }
+}
