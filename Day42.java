83c9aa667da104022de8380c0a955f60202d3f27
Andri <andrikurniawanm.12@gmail.com>
2024-10-13 20:39:44 +0800

    Day42

---
+++ Day42.java
@@ -0,0 +1,10 @@
+public class Day42 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 1;
+        b = 2;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+               
+    }
+}
