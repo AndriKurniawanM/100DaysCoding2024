9c8ae0a31d4313fafec5300da9f1619c8e918949
Andri <andrikurniawanm.12@gmail.com>
2024-10-14 19:20:34 +0800

    Day43

---
+++ Day43.java
@@ -0,0 +1,10 @@
+public class Day43 {
+    public static void main(String[] args) {
+        
+    int a,b;
+    a = 20;
+    b = 10;
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
