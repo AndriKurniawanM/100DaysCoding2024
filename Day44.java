2b91ea50ec70743d488c41c13d403f56df194b17
Andri <andrikurniawanm.12@gmail.com>
2024-10-15 21:14:55 +0800

    Day44

---
+++ Day44.java
@@ -0,0 +1,9 @@
+public class Day44 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 1;
+        b = 2;
+        boolean c = a <10 && b > 1;
+        System.out.println(c);
+    }
+}
