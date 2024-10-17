83b11171d15b401920e33d5ef96495c6250bd2e5
Andri <andrikurniawanm.12@gmail.com>
2024-10-17 21:59:53 +0800

    Day46

---
+++ Day46.java
@@ -0,0 +1,11 @@
+
+public class Day46 {
+    public static void main(String[] args) {
+        boolean a,b;
+        a = true;
+        b = !false;
+        System.out.println(!a);
+        System.out.println(b);
+    }
+    
+}
