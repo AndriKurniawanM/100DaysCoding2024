32d4bb8698951bc33e9c1decbda53148ce1f5f8f
Andri <andrikurniawanm.12@gmail.com>
2024-10-18 22:05:40 +0800

    Day47

---
+++ Day47.java
@@ -0,0 +1,9 @@
+public class Day47 {
+    public static void main(String[] args) {
+        int a= 1;
+        if (a > 0 && a <= 10) {
+            System.out.println("Sepuluh");
+        }
+    }  
+}
+ 
