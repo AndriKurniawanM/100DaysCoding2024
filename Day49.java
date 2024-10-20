d3e783af4daa03a4270eb4e97e1c8ff9c8e07aa9
Andri <andrikurniawanm.12@gmail.com>
2024-10-20 20:10:26 +0800

    Day49

---
+++ Day49.java
@@ -0,0 +1,11 @@
+
+public class Day49 {
+   public static void main(String[] args) {
+        int a= 12;
+        if (a > 0 && a <= 10) {
+            System.out.println("Sepuluh");
+        }else if (a > 10 && a <= 20){
+            System.out.println("DuaBelas");
+        }
+    }   
+}
