82e98301a8c10ab5629a8174fa0be615a2295699
Andri <andrikurniawanm.12@gmail.com>
2024-10-21 21:55:05 +0800

    Day50

---
+++ Day50.java
@@ -0,0 +1,12 @@
+
+public class Day50 {
+    public static void main(String[] args) {
+     int a = 0;
+        if (a > 0 && a <= 10) {
+            System.out.println("Sepuluh");
+        }else if (a > 10 && a <= 20){
+            System.out.println("DuaBelas");
+        }else{ System.out.println("0");
+        }
+    }
+}   
