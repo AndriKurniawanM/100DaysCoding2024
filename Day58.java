2c0b604fad921a7321bf9a47de0f43efde7deaca
Andri <andrikurniawanm.12@gmail.com>
2024-10-29 20:41:46 +0800

    Day58.java

---
+++ Day58.java
@@ -0,0 +1,11 @@
+public class day58 {
+    
+    public static void main(String[] args) {
+        int a = 0;
+        while (a>=-10) {
+        System.out.print(a + " ");
+            a--;
+        }
+ }
+    
+}
