67362cf9489f2b2f5ffb290264499d91cc6e3e6c
Andri <andrikurniawanm.12@gmail.com>
2024-10-30 21:15:00 +0800

    Day59.java

---
+++ Day59.java
@@ -0,0 +1,10 @@
+
+public class Day59 {
+    
+    public static void main(String[] args) {
+        char [] a = {'a','b','c','d'};
+        for (char z : a) {
+        System.out.print(z);
+    }
+    }
+}
