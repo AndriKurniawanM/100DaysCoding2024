03c6028778e4212c9b29809845305c885c0c0fbe
Andri <andrikurniawanm.12@gmail.com>
2024-11-05 21:07:21 +0800

    Nested Loop

---
+++ Day65.java
@@ -0,0 +1,12 @@
+public class Day65 {
+    public static void main(String[] args) {
+  //loop luar
+for (int i = 1; i <= 2; i++) {
+  System.out.println("Luar: " + i); // dieksekusi 2 kali
+  //loop dalam
+  for (int j = 1; j <= 3; j++) {
+    System.out.println(" Dalam: " + j); //dieksekusi 6 kali (2 * 3)
+            }
+        }
+    }
+}
