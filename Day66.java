b345c21a44cfb58d9b7a8f7f70d4e48442fa2a02
Andri <andrikurniawanm.12@gmail.com>
2024-11-07 21:17:37 +0800

    Pola persegi

---
+++ Day66.java
@@ -0,0 +1,19 @@
+
+public class Day66 {
+    public static void main(String[] args) {
+        //Persegi 3 x 3  
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println("");
+        }
+    }
+}
+        //persegi panjang 3 x 2
+                //for (int i = 1; i <= 3; i++) {
+            //for (int j = 1; j <= 2; j++) {
+                //System.out.print("*");
+           // } System.out.println(""); 
+ 
+
