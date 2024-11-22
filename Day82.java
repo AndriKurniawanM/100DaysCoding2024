3f43070a6e2f2829685ee320b35bdcafbc37051a
Andri <andrikurniawanm.12@gmail.com>
2024-11-22 22:22:52 +0800

    Method pembulatan angka

---
+++ Day82.java
@@ -0,0 +1,8 @@
+public class Day82 {
+    public static void main(String[] args) {
+    System.out.println(Math.ceil(0.6)); //langit 
+    System.out.println(Math.floor(0.6)); //lantai
+    System.out.println(Math.round(0.6)); //bulat
+    
+    }
+}
