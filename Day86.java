0163e4b647ae58a4f3a86da4bd5fa487e4a08bb8
Andri <andrikurniawanm.12@gmail.com>
2024-11-26 23:18:14 +0800

    Cetak elemen pada array

---
+++ Day86.java
@@ -0,0 +1,12 @@
+public class Day86 {
+    public static void main(String[] args) {
+        // Membuat dan mengisi array dengan nilai-nilai
+        int[] isi = {10, 20, 30, 40, 50};
+
+        // Mencetak elemen array menggunakan loop
+        System.out.println("Elemen-elemen pada array adalah:");
+        for (int output : isi) {
+            System.out.println(output);
+        }
+    }
+}
